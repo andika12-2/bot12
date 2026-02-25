30844088433c71fade0d57ac0352f77fee2355b2
andika12-2 <dikazamaman@gmail.com>
2026-02-25 16:16:50 +0700

    Add files via upload

---
+++ setting/config.js
@@ -0,0 +1,19 @@
+// Module
+const fs = require('fs')
+
+global.connect = true
+//
+global.publicX = true 
+//
+global.owner = ['6282336987563'] 
+//
+//System Bot Settings
+global.prefa = ['','!','.',',','🐤','🗿'] // Prefix // Not Change
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
